a1291506e405ac2d0a225eef74694a3291988d2c
David Hartveld <david@hartveld.com>
2013-07-09 15:23:00 -0500

    Added simplest possible APM usage example - callback version.

---
+++ Test/SimpleAPMCase.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test
+{
+    class SimpleAPMCase
+    {
+        public void FireAndForget()
+        {
+            var request = WebRequest.Create("http://www.google.com/");
+
+            request.BeginGetResponse(new AsyncCallback(CallBack), request);
+        }
+
+        private void CallBack(IAsyncResult result)
+        {
+            var request = (WebRequest)result.AsyncState;
+            request.EndGetResponse(result);
+        }
+    }
+}
