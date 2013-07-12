e40dc8fcc402248246a8a847b9df8ce91506a273
David Hartveld <david@hartveld.com>
2013-07-12 16:30:01 -0500

    Challenges project containing an initial challenging refactoring situation.

---
+++ Challenges/CatchBlockPropagationChallenge.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Challenges
+{
+    // In this example, the response variable in the Callback method should be made a parameter, 
+    // and the EndGetResponse call should be moved to the EntryPoint method. However, EndGetResponse
+    // might throw a WebException, so the catch block should be copied to the EntryPoint method.
+    // But unfortunately, the catch block references identifiers from the Callback method scope that
+    // encloses the try-catch statement, so it cannot be easily moved, without taking measures to
+    // make those references available in EntryPoint as well.
+
+    class CatchBlockPropagationChallenge
+    {
+        public static void EntryPoint()
+        {
+            var request = WebRequest.Create("url://...");
+            request.BeginGetResponse(Callback, request);
+        }
+
+        private static void Callback(IAsyncResult result)
+        {
+            var request = (WebRequest)result.AsyncState;
+            var x = 0;
+
+            try
+            {
+                var response = request.EndGetResponse(result);
+
+                // Do something with response, which might cause a WebException to be thrown.
+            }
+            catch (WebException e)
+            {
+                // Do something with x
+            }
+        }
+    }
+}
---
+++ Challenges/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Challenges
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, world!");
+            Console.WriteLine(" -- This project contains challenges for the implementation of the refactoring tool"););
+            Console.WriteLine(" -- See the other class source files for examples");
+        }
+    }
+}
