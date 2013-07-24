6e7af729c51b8e46a67f939f72fbb3d5bd64eebe
David Hartveld <david@hartveld.com>
2013-07-24 14:05:07 -0500

    Introduced abstract base test class that does all the hard work.

---
+++ Refactoring-Tests/APMToAsyncAwaitRefactoringTestBase.cs
@@ -0,0 +1,62 @@
+﻿using NUnit.Framework;
+using Refactoring;
+using Roslyn.Compilers;
+using Roslyn.Compilers.CSharp;
+
+namespace Refactoring_Tests
+{
+    /// <summary>
+    /// Base class for APM-to-async/await refactoring testing.
+    /// For clarity, use a single test class per test case.
+    /// </summary>
+    public class APMToAsyncAwaitRefactoringTestBase
+    {
+        // ReSharper disable InconsistentNaming
+        private static readonly MetadataReference mscorlib = MetadataReference.CreateAssemblyReference("mscorlib");
+        private static readonly MetadataReference system = MetadataReference.CreateAssemblyReference("system");
+        // ReSharper restore InconsistentNaming
+
+        /// <summary>
+        /// Find the invocation expression statement of the APM BeginXxx method call that must be refactored in the given compilation unit.
+        /// </summary>
+        /// <param name="syntax">The compilation unit in which the invocation expression must be found.</param>
+        /// <returns>The invocation expression statement.</returns>
+        public delegate ExpressionStatementSyntax StatementFinder(CompilationUnitSyntax syntax);
+
+        /// <summary>
+        /// Assert that given original code containing both the BeginXxx method
+        /// call, callback method declaration and EndXxx method call, is
+        /// correctly refactored to the given refactored code statement. The
+        /// statementFinder is used to return the APM invocation expression
+        /// statement that must be refactored.
+        /// </summary>
+        /// <param name="originalCode">The original code to refactor.</param>
+        /// <param name="refactoredCode">The refactored code to check against.</param>
+        /// <param name="statementFinder">The delegate that returns the APM BeginXxx
+        /// invocation expression statement that must be refactored.</param>
+        protected static void AssertThatOriginalCodeIsRefactoredCorrectly(string originalCode, string refactoredCode, StatementFinder statementFinder)
+        {
+            // Parse given original code
+            var originalSyntaxTree = SyntaxTree.ParseText(originalCode);
+            var originalSyntax = originalSyntaxTree.GetRoot();
+            var originalCompilation = Compilation.Create(
+                "OriginalCompilation",
+                syntaxTrees: new[] { originalSyntaxTree },
+                references: new[] { mscorlib, system }
+                );
+            var originalSemanticModel = originalCompilation.GetSemanticModel(originalSyntaxTree);
+
+            var apmInvocation = statementFinder(originalSyntax);
+
+            // Parse given refactored code
+            var refactoredSyntaxTree = SyntaxTree.ParseText(refactoredCode);
+            var refactoredSyntax = refactoredSyntaxTree.GetRoot();
+
+            // Perform actual refactoring
+            var actualRefactoredSyntax = originalSyntax.RefactorAPMToAsyncAwait(apmInvocation, originalSemanticModel);
+
+            Assert.That(actualRefactoredSyntax, Is.EqualTo(refactoredSyntax));
+            Assert.That(actualRefactoredSyntax.ToString(), Is.EqualTo(refactoredSyntax.ToString()));
+        }
+    }
+}
