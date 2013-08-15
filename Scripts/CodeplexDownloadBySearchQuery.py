faa4b71c03c737654dfa602b912834f125f9e714
Semih Okur <semih.okur@gmail.com>
2013-08-15 17:57:23 -0500

    New scripts for downloading apps

--- Scripts/CodeplexDownloadBySearchQuery.py
+++ Scripts/CodeplexDownloadBySearchQuery.py
@@ -1,40 +1,40 @@
 import os
-import mechanize
 import re
 from urllib2 import urlopen, URLError, HTTPError
 
 
-dir= "/Volumes/Data/CodeplexPhoneApps/"
+dir= "/Volumes/Data/CodeCorpus/"
 
-def dlfile(url):
+def dlfile(name):
     # Open the url
     try:
-		
-		name= url.split("/")[2].split(".")[0]
-		project_dir= dir+ name
-		
-		if not os.path.exists(project_dir):
-			os.makedirs(project_dir)
-			
-			f = urlopen(url+ "/SourceControl/BrowseLatest");
-			id=0
-			for line in f.readlines():
-				if "\"changesetId\"" in line:
-					id= line.split("\"changesetId\"")[1].split("\"")[1]
-			
-			if id==0:
-				f = open('undownloaded.txt', 'a')
-				f.write(name+'\n')
-				f.close()
-			else:
-				downloadUrl= "http://download-codeplex.sec.s-msft.com/Download/SourceControlFileDownload.ashx?ProjectName="+ name +"&changeSetId="+ id
-				f= urlopen(downloadUrl)
-				with open(project_dir+"/archive.zip", "wb") as local_file:
-					local_file.write(f.read())
+                
+                
+                project_dir= dir+ name
+                
+                if not os.path.exists(project_dir):
+                        
+                        
+                        f = urlopen("http://" +name + ".codeplex.com/SourceControl/BrowseLatest");
+                        id=0
+                        for line in f.readlines():
+                                if "\"changesetId\"" in line:
+                                        id= line.split("\"changesetId\"")[1].split("\"")[1]
+                        
+                        if id==0:
+                                f = open('undownloaded.txt', 'a')
+                                f.write(name+'\n')
+                                f.close()
+                        else:
+                                os.makedirs(project_dir)
+                                downloadUrl= "http://download-codeplex.sec.s-msft.com/Download/SourceControlFileDownload.ashx?ProjectName="+ name +"&changeSetId="+ id
+                                f= urlopen(downloadUrl)
+                                with open(project_dir+"/archive.zip", "wb") as local_file:
+                                        local_file.write(f.read())
 
-		else:
-			print "already downloaded"
-		
+                else:
+                        print "already downloaded"
+                
     #handle errors
     except HTTPError, e:
         print "HTTP Error:", e.code, url
@@ -44,26 +44,60 @@ def dlfile(url):
 
 
 def main():
-	address= "http://www.codeplex.com/site/search?query=&sortBy=Title&tagName=%2cWindows%20Phone%208%2c&licenses=|&refinedSearch=true&size=100&page="
-	
-	list= []
-	a=1
-	letsGet=False
-	
-	for page in range(0,2):
-		website = urlopen(address+str(page))
-		for line in website.readlines():
-			if "<h3>" in line:
-				project_url= line.split("\"")[1]
-				list.append(project_url)
-				print str(a)+" :"+project_url
-				a+=1
-	
-	i=1
-	for url in list:
-		print str(i)+ "-downloading " + url
-		dlfile(url)
-		i=i+1
+        address= "http://www.codeplex.com/site/search?query=windows%20phone&sortBy=Relevance&licenses=|&refinedSearch=true&size=100&page="
+        f = open("codeplexapps.txt", "wb")
+        
+        list= []
+        a=1
+        letsGet=False
+        
+        nameIsFound= False
+        name=""
+        desc=""
+        updated=""
+        for page in range(0,11):
+                website = urlopen(address+str(page))
+                for line in website.readlines():
+                        if "<h3>" in line:
+                            name= line.split("\"")[1].split("/")[2].split(".")[0]
+                            nameIsFound= True
+                            
+                        if "<p>" in line and nameIsFound:
+                            desc= line.replace('<p>','').replace('</p>','').replace('"','').replace("<span class='HighlightItem'>",'').replace('</span>', '').replace(',',' ')
+                            
+                        if "<p class" in line and nameIsFound:
+                            temp= line.split('class="green">')
+                            views= temp[1].split('<')[0]
+                            downloads= temp[2].split('<')[0]
+                            startDate = line.split('title="')[1].split('"')[0]
+                            
+                            try:
+                                for line2 in urlopen("http://"+name+".codeplex.com/SourceControl/list/changesets").readlines():
+                                    if '<span class="smartDate' in line2:
+                                        updated = line2.split('title="')[1].split('"')[0]
+                                f.write(name + "," + updated + "," + startDate + "," + views + "," + downloads + "," +desc)
+                                print str(a) + " " + name
+                            except Exception:
+                                print str(a) + " " + name + " *******"
+                                pass
+                            nameIsFound=False
+                            a+=1
+        
+        f.close()
+        i=1
+        #for url in list:
+        #       print str(i)+ "-downloading " + url
+        #       dlfile(url)
+        #       i=i+1
 
 
-main()
+def read():
+    f = open("codeplexapps.txt", "r")
+    i=1
+    for line in f.readlines():
+        name = line.split(",")[0]
+        if name != "name":
+            print str(i) + " "+ name
+            dlfile(name)
+            i+=1
+read()
