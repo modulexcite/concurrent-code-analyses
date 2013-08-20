5f1f107517ecfd3e9b07ad177b0f2c17970b97af
David Hartveld <david@hartveld.com>
2013-08-19 17:51:28 -0700

    Added Refactoring-BatchTool project.

---
+++ Refactoring-BatchTool/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Refactoring_BatchTool
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Hello, world!");
+
+            Console.WriteLine("Press any key to quit ...");
+            Console.ReadKey();
+        }
+    }
+}
