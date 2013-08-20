be837a7878d3869a523da506bbb20d7c0b9af9f3
David Hartveld <david@hartveld.com>
2013-08-20 14:58:52 -0700

    WIP: Batch refactoring tool.

--- Refactoring-BatchTool/Program.cs
+++ Refactoring-BatchTool/Program.cs
@@ -1,15 +1,73 @@
 ﻿using System;
+using System.Linq;
+using NLog;
+using Refactoring;
+using Roslyn.Compilers.CSharp;
+using Roslyn.Services;
 
 namespace Refactoring_BatchTool
 {
-    class Program
+    internal class Program
     {
+        private static readonly Logger Logger = LogManager.GetCurrentClassLogger();
+
+        private const string SolutionFile = @"C:\Users\david\Projects\UIUC\APM-to-AA-Test\APM-to-AA-Test.sln";
+
         static void Main()
         {
-            Console.WriteLine("Hello, world!");
+            Logger.Info("Hello, world!");
 
-            Console.WriteLine("Press any key to quit ...");
+            var solution = TryLoadSolution(SolutionFile);
+
+            if (solution == null)
+            {
+                Logger.Error("Failed to load solution file: {0}", SolutionFile);
+                return;
+            }
+
+            foreach (var project in solution.Projects)
+            {
+                Logger.Info("Project: {0}", project.FilePath);
+            }
+
+            var trees = solution.Projects
+                                .SelectMany(project => project.Documents)
+                                .Where(document => document.FilePath.EndsWith(".cs"))
+                                .Select(document => document.GetSyntaxTree())
+                                .OfType<SyntaxTree>();
+
+            foreach (var tree in trees)
+            {
+                var compilation = CompilationUtils.CreateCompilation(tree);
+                var model = compilation.GetSemanticModel(tree);
+
+                var searcher = new BeginXxxSearcher(model);
+                searcher.Visit(tree.GetRoot());
+
+                var beginXxxSyntax = searcher.BeginXxxSyntax;
+
+                if (beginXxxSyntax != null)
+                {
+                    // TODO
+                }
+            }
+
+            Console.WriteLine(@"Press any key to quit ...");
             Console.ReadKey();
+        }
+
+        private static ISolution TryLoadSolution(string solutionPath)
+        {
+            try
+            {
+                return Solution.Load(solutionPath);
+            }
+            catch (Exception ex)
+            {
+                Logger.Warn("Solution not analyzed: {0}: Reason: {1}", solutionPath, ex.Message);
+
+                return null;
+            }
         }
     }
 }
