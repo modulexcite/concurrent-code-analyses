9c9f811a36d22b8448f0c146aed6ca826e8a2cb1
David Hartveld <david@hartveld.com>
2013-09-05 18:05:39 -0700

    Added expected refactored code string.

--- Refactoring-Tests/APMBeginInLocalDeclarationStatementFailsWhenIdentifiersCollideTest.cs
+++ Refactoring-Tests/APMBeginInLocalDeclarationStatementFailsWhenIdentifiersCollideTest.cs
@@ -9,13 +9,13 @@ namespace Refactoring_Tests
         public void TestThatAPMBeginRewritingUsesNonCollidingIdentifierForLambdaParameter()
         {
             AssertThatOriginalCodeIsRefactoredCorrectly(
-                OriginalCodeWithUsedAsyncState,
-                "",
+                OriginalCode,
+                RefactoredCode,
                 FirstBeginInvocationFinder("request.BeginGetResponse")
             );
         }
 
-        private const string OriginalCodeWithUsedAsyncState = @"using System;
+        private const string OriginalCode = @"using System;
 using System.Net;
 
 namespace TextInput
@@ -32,6 +32,31 @@ namespace TextInput
         {
             var request = (WebRequest)result.AsyncState;
             var response = request.EndGetResponse(result);
+
+            DoSomethingWithRequestAndResponse(request, response);
+        }
+
+        private static void DoSomethingWithRequestAndResponse(WebRequest request, WebResponse response) { }
+    }
+}";
+
+        private const string RefactoredCode = @"using System;
+using System.Net;
+
+namespace TextInput
+{
+    class SimpleAPMCase
+    {
+        public async void FireAndForget()
+        {
+            var request = WebRequest.Create(""http://www.microsoft.com/"");
+            var task = request.GetResponseAsync();
+            await Callback(task, request).ConfigureAwait(false);
+        }
+
+        private async Task Callback(Task<WebResponse> task, WebRequest request)
+        {
+            var response = await task.ConfigureAwait(false);
 
             DoSomethingWithRequestAndResponse(request, response);
         }
